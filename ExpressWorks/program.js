465360d870116ceb7dfda32a4450b0d124f20c82
Hubert <huberthflin@gmail.com>
2018-10-26 09:12:06 -0700

    completed STATIC

--- ExpressWorks/program.js
+++ ExpressWorks/program.js
@@ -1,7 +1,17 @@
 //HELLO WORLD
+// var express = require('express');
+// var app = express();
+// app.get('/home', (req, res) => {
+//   res.end('Hello World!');
+// });
+// app.listen(process.argv[2]);
+
+//STATIC
 var express = require('express');
 var app = express();
-app.get('/home', (req, res) => {
-  res.end('Hello World!');
-});
-app.listen(process.argv[2]);
+var path = require('path');
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+const port = process.argv[2];
+test = path.join(__dirname, 'public');
+console.log(test);
+app.listen(port);
