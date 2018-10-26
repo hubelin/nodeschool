a6cd76932592cad03c003c23172c695d31f21770
Hubert <huberthflin@gmail.com>
2018-10-26 09:40:33 -0700

    completed PUG

--- ExpressWorks/program.js
+++ ExpressWorks/program.js
@@ -7,11 +7,25 @@
 // app.listen(process.argv[2]);
 
 //STATIC
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+// app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+// const port = process.argv[2];
+// test = path.join(__dirname, 'public');
+// console.log(test);
+// app.listen(port);
+
+//PUG
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
-const port = process.argv[2];
-test = path.join(__dirname, 'public');
-console.log(test);
-app.listen(port);
+
+app.set('views', process.argv[3]);
+app.set('view engine', 'pug');
+
+app.get('/home', (req, res) => {
+  res.render('index', { date: new Date().toDateString() });
+});
+
+app.listen(process.argv[2]);
