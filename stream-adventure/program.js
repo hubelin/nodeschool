09d8e49a43f4acdcbf202ffac6aa9fdd9c15a844
Hubert <huberthflin@gmail.com>
2018-10-25 17:16:44 -0700

    completed stream-adventure HTTP SERVER and HTTP CLIENT

---
+++ stream-adventure/program.js
@@ -0,0 +1,31 @@
+// //HTTP SERVER
+// var http = require('http');
+// var fs = require('fs');
+// var through = require('through2');
+// var stream = through(
+//   function write(buffer, encoding, next) {
+//     this.push(buffer.toString().toUpperCase());
+//     next();
+//   },
+//   function end(done) {
+//     done();
+//   }
+// );
+
+// http
+//   .createServer((req, res) => {
+//     if (req.method === 'POST') {
+//       req.pipe(stream).pipe(res);
+//     } else {
+//       req.end();
+//     }
+//   })
+//   .listen(process.argv[2]);
+
+//HTTP CLIENT
+// var http = require('http');
+var request = require('request');
+
+let url = 'http://localhost:8099';
+let r = request.post(url);
+process.stdin.pipe(r).pipe(process.stdout);
