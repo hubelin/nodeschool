c8a2df0b63291ab9824c412a2fe94715c559d145
Hubert <huberthflin@gmail.com>
2018-10-27 05:29:38 -0700

    completed STYLISH CSS

--- ExpressWorks/program.js
+++ ExpressWorks/program.js
@@ -31,17 +31,28 @@
 // app.listen(process.argv[2]);
 
 //GOOD OLD FORM
+// var express = require('express');
+// var app = express();
+// var bodyparser = require('body-parser');
+
+// app.use(bodyparser.urlencoded({ extended: false }));
+
+// app.post('/form', (req, res) => {
+//   result = req.body.str
+//     .split('')
+//     .reverse()
+//     .join('');
+//   res.end(result);
+// });
+// app.listen(process.argv[2]);
+
+//STYLISH CSS
 var express = require('express');
 var app = express();
-var bodyparser = require('body-parser');
-
-app.use(bodyparser.urlencoded({ extended: false }));
-
-app.post('/form', (req, res) => {
-  result = req.body.str
-    .split('')
-    .reverse()
-    .join('');
-  res.end(result);
-});
+app.use(
+  require('stylus').middleware(
+    process.argv[3] || path.join(__dirname, 'public')
+  )
+);
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 app.listen(process.argv[2]);
