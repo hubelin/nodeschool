d9c1316b776d2052acdfc05d942ef50e4d4b0ba4
Hubert <huberthflin@gmail.com>
2018-10-28 07:43:48 -0700

    completed Reject a Promise

--- PIWH/program.js
+++ PIWH/program.js
@@ -3,15 +3,30 @@
 // }, 300);
 
 // Fulfilling a Promise
-'use strict';
+// 'use strict';
+// require('es6-promise');
+// //create promise
+// var promise = new Promise((fulfill, reject) => {
+//   //fulfill promise
+//   setTimeout(() => {
+//     fulfill('FULFILLED!');
+//   }, 300);
+// }).then(data => {
+//   console.log(data);
+// });
+
+// Reject a Promise
+('use strict');
 require('es6-promise');
 
-//create Promise
 var promise = new Promise((fulfill, reject) => {
-  //fulfill promise
   setTimeout(() => {
-    fulfill('FULFILLED!');
+    reject(new Error('REJECTED!'));
   }, 300);
-}).then(data => {
-  console.log(data);
 });
+
+var onReject = error => {
+  console.log(error.message);
+};
+
+promise.then(null, onReject);
