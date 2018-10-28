da1d9bed0e1662578a2a712b337a3239d26d353e
Hubert <huberthflin@gmail.com>
2018-10-28 07:36:44 -0700

    completed Fulfill A Promise

--- PIWH/program.js
+++ PIWH/program.js
@@ -1,3 +1,17 @@
-setTimeout(() => {
-  console.log('TIMED OUT!');
-}, 300);
+// setTimeout(() => {
+//   console.log('TIMED OUT!');
+// }, 300);
+
+// Fulfilling a Promise
+'use strict';
+require('es6-promise');
+
+//create Promise
+var promise = new Promise((fulfill, reject) => {
+  //fulfill promise
+  setTimeout(() => {
+    fulfill('FULFILLED!');
+  }, 300);
+}).then(data => {
+  console.log(data);
+});
